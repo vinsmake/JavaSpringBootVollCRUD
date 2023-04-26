41f808174047eac06c3ff0342e6444074013f532
vinsmake <ps.enrique.e@gmail.com>
2023-04-25 20:48:37 -0600

    chore | Validation data added.

--- src/main/java/voll/api/direccion/DatosDireccion.java
+++ src/main/java/voll/api/direccion/DatosDireccion.java
@@ -1,8 +1,17 @@
 package voll.api.direccion;
 
-
+import jakarta.validation.constraints.NotBlank;
 
 //creates a normal class but when we run the code
-public record DatosDireccion(String calle, String distrito, String ciudad, String numero, String complemento) {
+public record DatosDireccion(
+    @NotBlank String calle, 
+    
+    @NotBlank String distrito, 
+    
+    @NotBlank String ciudad, 
+    
+    @NotBlank String numero, 
+    
+    @NotBlank String complemento) {
     
 }
--- src/main/java/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/voll/api/medico/DatosRegistroMedico.java
@@ -1,7 +1,21 @@
 package voll.api.medico;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import voll.api.direccion.*;
 
 
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion direccion) {
+public record DatosRegistroMedico(
+    //with validation dependency we can validate the data, NotBlank is NotData
+    @NotBlank String nombre,
+
+    @NotBlank String email,
+    //it need to be a number of 4 to 10 digits
+    @NotBlank @Pattern(regexp = "\\d{4,10}") String documento, 
+
+    @NotNull Especialidad especialidad, 
+    //we use NotNull when it's an object
+    @NotNull @Valid DatosDireccion direccion) {
     
 }
